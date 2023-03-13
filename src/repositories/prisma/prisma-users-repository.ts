81be3bff70d0fd013ab69cb8d6fda34c22bc9e3f
João <jv.dossantos012@gmail.com>
2023-03-13 01:51:46 -0300

    Implementação dos repositories, use-cases e controllers

---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,23 @@
+import { UsersRepository } from '../users-repository'
+import { Prisma } from '@prisma/client'
+import { prisma } from '../../lib/prisma'
+
+export class PrismaUsersRepository implements UsersRepository {
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    })
+
+    return user
+  }
+}
